f692e7c5f7fce291f575f14429d9808239dbee22
Mario FERRALI <marioferrali538@gmail.com>
2025-07-14 11:33:42 +0200

    chore(config): update assets and Tailwind config for new block setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   content: [
-    './views/**/*.twig',
-    './acf-blocks/**/*.twig',
+    './*.php',
+    './functions.php',
     './app/**/*.php',
-    './*.php'
+    './acf-blocks/**/*.php',
+    './acf-blocks/**/*.twig',
+    './views/**/*.twig',
   ],
   theme: {
     extend: {
       spacing: {
+        '8': '2rem',
+        '4': '1rem',  
         '16xl': '4rem',
         '20xl': '5rem',
         '12lg': '3rem',
@@ -15,4 +19,16 @@ module.exports = {
     },
   },
   plugins: [],
+    safelist: [
+    'text-left',
+    'text-center',
+    'text-right',
+    'bg-black',
+    'bg-white',
+    'bg-gray-100',
+    'py-8',
+    'px-4',
+    'pt-12',
+    'pb-12',
+  ]
 }
