078f8920a772738bfe1fa7164ed2efc141955dc1
Mario FERRALI <marioferrali538@gmail.com>
2025-07-15 18:18:05 +0200

    chore(config): update Tailwind config for block layout changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        marquee: 'marquee linear infinite',
+      },
     },
   },
   safelist: [
