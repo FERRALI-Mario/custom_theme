ba744eb57e9350df09c7b64b8fb957165954002f
Mario FERRALI <marioferrali538@gmail.com>
2025-07-17 13:39:46 +0200

    chore(theme): register new social-links block in Theme

--- app/Core/Theme.php
+++ app/Core/Theme.php
@@ -41,8 +41,12 @@ class Theme
         wp_enqueue_script('theme-js', $js, [], filemtime(get_template_directory() . '/assets/js/main.js'), true);
         wp_enqueue_style('theme-css', $css, [], filemtime(get_template_directory() . '/assets/css/tailwind.css'));
 
+        /** MAPS */
         wp_enqueue_style('leaflet-css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', [], '1.9.4');
         wp_enqueue_script('leaflet-js', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', [], '1.9.4', true);
+
+        /** FONTAWESOME */
+        wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css', [], null);
     }
 
     public static function bootTimber(): void
